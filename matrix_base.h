debfd6be67accf6b2f3541135efe2d84c970a373
Oleksandr Pyshovets <mr.pesh@outlook.com>
2018-09-07 09:01:45 +0300

    Basic operations with matrices and scalars

--- matrix_base.h
+++ matrix_base.h
@@ -6,12 +6,14 @@ template <typename T, uint_fast16_t rows, uint_fast16_t columns>
 struct Matrix :
     std::array <T, rows * columns>
 {
-    using Type = Matrix<T, rows, columns>;
+    static_assert(rows > 0 && columns > 0, "Matrix can't be of zero or negative size");
+    
+    typedef Matrix<T, rows, columns> Type;
+    typedef Matrix<T, columns, rows> Transposed;
 
-    template <typename ResultType = Matrix<T, columns, rows> >
-    ResultType transpose()
+    Transposed transpose()
     {
-        ResultType result;
+        Transposed result;
         #pragma omp parallel for
         for (uint_fast16_t n = 0; n < rows * columns; ++n) {
             const auto i = n / rows;
@@ -20,5 +22,32 @@ struct Matrix :
         }
 
         return result;
+    }
+
+    template <typename Number>
+    Type& operator*=(const Number scalar) {
+        static_assert(std::is_arithmetic<Number>::value);
+        #pragma omp parallel for
+        for (uint_fast16_t i = 0; i < size(); ++i) {
+            (*this)[i] *= scalar;
+        }
+        return *this;
+    }
+
+    template <typename Number>
+    inline Type operator*(const Number scalar) {
+        return Type(*this) *= scalar;
+    }
+
+    Type& operator+=(const Type &other) {
+        #pragma omp parallel for
+        for (uint_fast16_t i = 0; i < size(); ++i) {
+            (*this)[i] += other[i];
+        }
+        return *this;
+    }
+
+    inline Type& operator+(const Type &other) {
+        return Type(*this) += other;
     }
 };
