5d8f48fec97352331aab88f847b87095004c22ee
Oleksandr <mr.pesh@outlook.com>
2018-09-07 17:34:26 +0300

    Parallelism optional support
    Added usage of parallel execution depending on the compiler type and options:
    * Usage of parallel algorithms if <execution> module is available
    * Otherwise try to use OpenMP backend if available
    If compiler doesn't support any feature from the mentioned above, all operations will be performed sequentially.

--- matrix_base.h
+++ matrix_base.h
@@ -2,6 +2,10 @@
 
 #include <array>
 
+#ifdef __cpp_lib_execution
+# include <execution>
+#endif
+
 template <typename T, uint_fast16_t rows, uint_fast16_t columns>
 struct Matrix :
     std::array <T, rows * columns>
@@ -25,29 +29,48 @@ struct Matrix :
     }
 
     template <typename Number>
-    Type& operator*=(const Number scalar) {
+    Type& operator*=(const Number scalar)
+    {
         static_assert(std::is_arithmetic<Number>::value);
+
+    #ifdef __cpp_lib_execution
+        std::for_each(
+            std::execution::par_unseq,
+            this->begin(), this->end(), [=] (T &value) { return value *= scalar; }
+        );
+    #else
         #pragma omp parallel for
-        for (uint_fast16_t i = 0; i < size(); ++i) {
+        for (uint_fast16_t i = 0; i < this->size(); ++i) {
             (*this)[i] *= scalar;
         }
+    #endif
         return *this;
     }
 
     template <typename Number>
-    inline Type operator*(const Number scalar) {
+    inline Type operator*(const Number scalar)
+    {
         return Type(*this) *= scalar;
     }
 
-    Type& operator+=(const Type &other) {
+    Type& operator+=(const Type &other)
+    {
+    #ifdef __cpp_lib_execution
+        std::transform(
+            std::execution::par_unseq,
+            this->cbegin(), this->cend(), other.cbegin(), this->begin(), std::plus<T>()
+        );
+    #else
         #pragma omp parallel for
-        for (uint_fast16_t i = 0; i < size(); ++i) {
+        for (uint_fast16_t i = 0; i < this->size(); ++i) {
             (*this)[i] += other[i];
         }
+    #endif
         return *this;
     }
 
-    inline Type& operator+(const Type &other) {
+    inline Type& operator+(const Type &other)
+    {
         return Type(*this) += other;
     }
 };
