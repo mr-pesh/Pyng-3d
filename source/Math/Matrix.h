278fcaf695d9f6cdda70856ede0a31a5203ede5e
Oleksandr Pyshovets <mr.pesh@outlook.com>
2018-11-27 14:11:17 +0200

    Synced Matrix test with OpenGL types
    Added `begin()` and `end()` implementations for glm::mat types

--- source/Math/Matrix.h
+++ source/Math/Matrix.h
@@ -9,6 +9,23 @@
 template <typename T, int rows, int columns, glm::qualifier precision = glm::defaultp>
 using Matrix = glm::mat<columns, rows, T, precision>;
 
+#include <glm/gtc/type_ptr.hpp>
+
+namespace std
+{
+    template <typename T>
+    constexpr auto begin(const T& glm_type) noexcept
+    {
+        return glm::value_ptr(glm_type);
+    }
+
+    template <typename T>
+    constexpr auto end(const T& glm_type) noexcept
+    {
+        return glm::value_ptr(glm_type) + glm_type.length();
+    }
+}
+
 #else
 
 template <typename T, int rows, int columns>
