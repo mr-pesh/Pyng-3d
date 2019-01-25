1930476da198016b7dba49f645390790f0003194
Oleksandr Pyshovets <mr.pesh@outlook.com>
2019-01-26 00:15:16 +0200

    Add implementation of policy-based UniquePointer type

---
+++ source/Utility/UniquePointer.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include "Global.h"
+
+#ifdef _MSC_VER
+# pragma warning(suppress: 4005)
+# define alloca(size)  _malloca(size)
+#elif defined (__GNUC__)
+# ifndef alloca
+# define alloca(size) __builtin_alloca(size)
+# endif
+#endif
+
+template <class T>
+struct HeapAllocationPolicy
+{
+    template <class ...Args>
+    FORCE_INLINE_S T* Create(Args ...args) noexcept (std::is_nothrow_constructible<T>::value)
+    {
+        return new T(args...);
+    }
+    FORCE_INLINE_S void Destroy(T *ptr) noexcept (std::is_nothrow_destructible<T>::value)
+    {
+        delete ptr;
+    }
+};
+
+template <class T>
+struct StackAllocationPolicy
+{
+    template <class ...Args>
+    FORCE_INLINE_S T* Create(Args ...args) noexcept (std::is_nothrow_constructible<T>::value)
+    {
+        return new (alloca(sizeof(T))) T(args...);
+    }
+
+    FORCE_INLINE_S void Destroy(T *ptr) noexcept
+    {
+#ifdef _MSC_VER
+        _freea((void*)ptr);
+#endif
+        UNUSED(ptr)
+    }
+};
+
+
+template <template <class> class AllocationPolicy, class T>
+class UniquePointer
+{
+    DISABLE_COPY(UniquePointer)
+
+public:
+    FORCE_INLINE UniquePointer(T *ptr) noexcept (std::is_nothrow_constructible<T>::value)
+        : data(ptr)
+    {  }
+    FORCE_INLINE ~UniquePointer() noexcept (std::is_nothrow_destructible<T>::value)
+    {
+        AllocationPolicy<T>::Destroy(data);
+    }
+    FORCE_INLINE UniquePointer(UniquePointer&&) noexcept = default;
+
+    T *get() const noexcept { return data; }
+
+    T *operator->() const noexcept { return get(); }
+
+    std::add_lvalue_reference_t<T> operator*() const noexcept
+    {
+        return *get();
+    }
+
+    explicit operator bool() const noexcept { return data; }
+
+private:
+    T *data;
+};
+
+
+template <template <class>
+          class AllocationPolicy, class Type, class ...Args>
+FORCE_INLINE UniquePointer<AllocationPolicy, Type> AllocateUnique(Args ...args) noexcept (std::is_nothrow_constructible<Type>::value)
+{
+    return UniquePointer<AllocationPolicy, Type>(AllocationPolicy<Type>::Create(args...));
+}
---
+++ testing/UniquePointer/UniquePointerTest.cpp
@@ -0,0 +1,49 @@
+#include <gtest/gtest.h>
+#include <Utility/UniquePointer.h>
+
+class UniquePointerUnitTest : public testing::Test {
+};
+
+struct A {
+    A(uint32_t a_, uint32_t g_) : a(a_), g(g_) { }
+    uint32_t a;
+    uint32_t g;
+};
+
+template <class T>
+void printF(const T &ptr)
+{
+    for (const auto &p : ptr) {
+        std::cout << "Address: " << &(*p) << std::endl;
+        std::cout << "[" << p->a << ',' << p->g << ']' << std::endl;
+    }
+}
+
+TEST_F(UniquePointerUnitTest, ManageTest)
+{   
+    UniquePointer<HeapAllocationPolicy, A> ptr1[] = {
+        AllocateUnique<HeapAllocationPolicy, A>(1, 51),
+        AllocateUnique<HeapAllocationPolicy, A>(2, 52),
+        AllocateUnique<HeapAllocationPolicy, A>(3, 53),
+        AllocateUnique<HeapAllocationPolicy, A>(4, 54),
+        AllocateUnique<HeapAllocationPolicy, A>(5, 55),
+    };
+
+    UniquePointer<StackAllocationPolicy, A> ptr2[] = {
+        AllocateUnique<StackAllocationPolicy, A>(6, 56),
+        AllocateUnique<StackAllocationPolicy, A>(7, 57),
+        AllocateUnique<StackAllocationPolicy, A>(8, 58),
+        AllocateUnique<StackAllocationPolicy, A>(9, 59),
+        AllocateUnique<StackAllocationPolicy, A>(10, 60),
+    };
+
+    printF(ptr1);
+    printF(ptr2);
+
+    ASSERT_TRUE(true);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
