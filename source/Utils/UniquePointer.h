8a11017574a884603b4b578f61f5797cd16c16ed
Oleksandr Pyshovets <mr.pesh@outlook.com>
2019-02-12 16:07:54 +0200

    Finish implementation of STL's unique_ptr interface

--- source/Utils/UniquePointer.h
+++ source/Utils/UniquePointer.h
@@ -71,16 +71,16 @@ public:
     typedef std::add_pointer_t<element_type> pointer;
     typedef std::add_lvalue_reference_t<element_type> reference;
 
-    FORCE_INLINE UniquePointer(element_type *ptr) noexcept (std::is_nothrow_constructible_v<element_type>)
-        : data_(ptr)
+    FORCE_INLINE UniquePointer(UniquePointer&&) noexcept = default;
+
+    FORCE_INLINE UniquePointer(pointer p = pointer()) noexcept (std::is_nothrow_constructible_v<element_type>)
+        : data_(p)
     {  }
 
     FORCE_INLINE ~UniquePointer() noexcept (std::is_nothrow_destructible_v<element_type>)
     {
         AllocationPolicy<T>::Destroy(data_);
     }
-
-    FORCE_INLINE UniquePointer(UniquePointer&&) noexcept = default;
 
     explicit operator bool() const noexcept
     {
@@ -130,10 +130,139 @@ public:
         std::swap(data_, other.data_);
     }
 
+    UniquePointer &operator=(UniquePointer&& other) noexcept = default;
+
+    UniquePointer &operator=(pointer p) noexcept { reset(p); return *this; }
+
+    UniquePointer &operator=(nullptr_t) noexcept { reset( ); return *this; }
+
 private:
     pointer data_;
 };
 
+
+template <class T1, template <class> class P1, class T2, template <class> class P2>
+inline bool operator==(const UniquePointer<T1,P1> &x, const UniquePointer<T2,P2> &y) noexcept
+{
+    return x.get() == y.get();
+}
+
+template <class T1, template <class> class P1, class T2, template <class> class P2>
+inline bool operator!=(const UniquePointer<T1, P1> &x, const UniquePointer<T2, P2> &y) noexcept
+{
+    return x.get() != y.get();
+}
+
+template <class T1, template <class> class P1, class T2, template <class> class P2,
+          typename pointer = std::common_type_t<UniquePointer<T1,P1>::pointer, UniquePointer<T2,P2>::pointer>
+          >
+inline bool operator<(const UniquePointer<T1, P1> &x, const UniquePointer<T2, P2> &y) noexcept
+{
+    return std::less<pointer>()(x.get(), y.get());
+}
+
+template <class T1, template <class> class P1, class T2, template <class> class P2>
+inline bool operator<=(const UniquePointer<T1, P1> &x, const UniquePointer<T2, P2> &y) noexcept
+{
+    return !(x < y);
+}
+
+template <class T1, template <class> class P1, class T2, template <class> class P2>
+inline bool operator>(const UniquePointer<T1, P1> &x, const UniquePointer<T2, P2> &y) noexcept
+{
+    return y < x;
+}
+
+template <class T1, template <class> class P1, class T2, template <class> class P2>
+inline bool operator>=(const UniquePointer<T1, P1> &x, const UniquePointer<T2, P2> &y) noexcept
+{
+    return !(y < x);
+}
+
+template <class T, template <class> class P>
+inline bool operator==(const UniquePointer<T,P> &x, nullptr_t) noexcept
+{
+    return !x;
+}
+
+template <class T, template <class> class P>
+inline bool operator==(nullptr_t, const UniquePointer<T,P> &x) noexcept
+{
+    return !x;
+}
+
+template <class T, template <class> class P>
+inline bool operator!=(const UniquePointer<T,P> &x, nullptr_t) noexcept
+{
+    return (bool)x;
+}
+
+template <class T, template <class> class P>
+inline bool operator!=(nullptr_t, const UniquePointer<T,P> &x) noexcept
+{
+    return (bool)x;
+}
+
+template <class T, template <class> class P,
+          typename pointer = UniquePointer<T, P>::pointer
+          >
+inline bool operator<(const UniquePointer<T,P> &x, nullptr_t) noexcept
+{
+    return std::less<pointer>()(x.get(), nullptr);
+}
+
+template <class T, template <class> class P,
+          typename pointer = UniquePointer<T,P>::pointer
+          >
+inline bool operator<(nullptr_t, const UniquePointer<T,P> &y) noexcept
+{
+    return std::less<pointer>()(nullptr, y.get());
+}
+
+template <class T, template <class> class P>
+inline bool operator<=(const UniquePointer<T,P> &x, nullptr_t) noexcept
+{
+    return !(nullptr < x);
+}
+
+template <class T, template <class> class P>
+inline bool operator<=(nullptr_t, const UniquePointer<T,P> &y) noexcept
+{
+    return !(y < nullptr);
+}
+
+template <class T, template <class> class P>
+inline bool operator>(const UniquePointer<T,P> &x, nullptr_t) noexcept
+{
+    return (nullptr < x);
+}
+
+template <class T, template <class> class P>
+inline bool operator>(nullptr_t, const UniquePointer<T,P> &y) noexcept
+{
+    return (y < nullptr);
+}
+
+template <class T, template <class> class P>
+inline bool operator>=(const UniquePointer<T,P> &x, nullptr_t) noexcept
+{
+    return !(x < nullptr);
+}
+
+template <class T, template <class> class P>
+inline bool operator>=(nullptr_t, const UniquePointer<T,P> &y) noexcept
+{
+    return !(nullptr < y);
+}
+
+template <class Char, class Traits,
+          class PointerType, template <class> class AllocationPolicy,
+          class Stream = std::basic_ostream<Char, Traits>
+          >
+inline Stream &operator<<(Stream &ostream, const UniquePointer<PointerType, AllocationPolicy> &ptr)
+{
+    return ostream << ptr.get();
+}
 
 template <class Type,
           template <class> class AllocationPolicy = HeapAllocationPolicy,
@@ -147,7 +276,8 @@ NODISCARD FORCE_INLINE UniquePointer<Type, AllocationPolicy> AllocateUnique(Args
 template <class Type,
           template <class> class AllocationPolicy = HeapAllocationPolicy,
           std::enable_if_t<std::is_array_v<Type> && std::extent_v<Type> == 0, int> = 0,
-          typename _Ty = std::remove_extent_t<Type>>
+          typename _Ty = std::remove_extent_t<Type>
+          >
 NODISCARD FORCE_INLINE UniquePointer<Type, AllocationPolicy> AllocateUnique(size_t size) noexcept (std::is_nothrow_constructible_v<_Ty>)
 {
     return UniquePointer<Type, AllocationPolicy>(AllocationPolicy<Type>::Create(size));
@@ -158,3 +288,21 @@ template<class Type,
          class... Args, std::enable_if_t<std::extent_v<Type> != 0, int> = 0
          >
 UniquePointer<Type, AllocationPolicy> AllocateUnique(Args&&...) = delete;
+
+namespace std
+{
+    template <class Type, template <class> class AllocationPolicy>
+    void swap(UniquePointer<Type, AllocationPolicy> &lhs, UniquePointer<Type, AllocationPolicy> &rhs) noexcept
+    {
+        lhs.swap(rhs);
+    }
+
+    template <class Type, template <class> class AllocationPolicy>
+    struct hash< UniquePointer<Type, AllocationPolicy> >
+    {
+        NODISCARD size_t operator()(const UniquePointer<Type, AllocationPolicy> &ptr) const noexcept
+        {
+            return hash<typename UniquePointer<Type, AllocationPolicy>::pointer>()(ptr.get());
+        }
+    };
+}
